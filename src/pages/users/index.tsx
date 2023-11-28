3afe117fd62d01b4892104da7664aed1523ee354
Emanuel <emanuelmarcolongo@gmail.com>
2023-11-28 14:56:20 -0300

    layout: small layout adjustments in users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,37 +1,38 @@
-import { useEffect, useState } from "react";
-import { FlatList, View } from "react-native";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import axios from "axios";
 import API_URL from "../../utils/Urls/API-URL";
 import UserCard from "./components/user-card";
 import { UserEntity } from "../../utils/Types/userType";
 import Header from "./components/header";
+import { UsersContainer, ListContainer } from "./styles";
 
 export default function UsersPage() {
   const [users, setUsers] = useState<UserEntity[]>([]);
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get(`${API_URL}/users`);
-        setUsers(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
 
-    fetchUsers();
-  }, []);
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/users`);
+      setUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  fetchUsers();
 
   return (
-    <View>
+    <UsersContainer>
       <Header />
 
-      <FlatList
-        data={users}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => {
-          return <UserCard userData={item}></UserCard>;
-        }}
-      />
-    </View>
+      <ListContainer>
+        <FlatList
+          data={users}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => {
+            return <UserCard userData={item}></UserCard>;
+          }}
+        />
+      </ListContainer>
+    </UsersContainer>
   );
 }
---
+++ src/pages/users/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { View } from "react-native";
+
+export const UsersContainer = styled(View)``;
+
+export const ListContainer = styled(View)`
+  margin-bottom: 150px;
+  padding: 16px;
+`;
